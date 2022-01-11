c452e58036972240145063992cfef28daf379e46
Caleb Horton <c.alex.horton@gmail.com>
2022-01-11 12:05:19 -0600

    fixed security error

--- src/context/weather/WeatherReducer.js
+++ src/context/weather/WeatherReducer.js
@@ -1,5 +1,10 @@
 const weatherReducer = (state, action) => {
     switch (action.type) {
+        case 'GET_LOCATION':
+            return {
+                ...state,
+                locations: [...state.locations, action.payload]
+            }
         default:
             return state
     }
